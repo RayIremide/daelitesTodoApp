fc9fe698d7b79be80f95224d8d40dce3375db1e1
Iremide Olatunji <olatunjiremide@gmail.com>
2023-11-10 05:54:36 +0100

    Improve Rate Limit

--- app.js
+++ app.js
@@ -25,8 +25,8 @@ mongoDB.connectToMongoDB()
 
 // Rate Limit
 const limiter = rateLimit({
-	windowMs: 0.5 * 60 * 1000, // 15 minutes
-	max: 4, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 20, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
 	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 })
